85fc0a60de63874f00474c3eb05d8f52c5e8c661
FedericaCampo97 <federica.campo97@gmail.com>
2023-09-06 14:10:31 +0200

    start of exercise

---
+++ assets/main.js
@@ -0,0 +1,8 @@
+/*Utilizzando i dati forniti, creare un array di 
+oggetti per rappresentare i membri del team. 
+Ogni membro è caratterizzato dalle seguenti 
+informazioni: nome, ruolo e foto.*/
+
+/*Creare l’array di oggetti con le 
+informazioni fornite.*/
+
